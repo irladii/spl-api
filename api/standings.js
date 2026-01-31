3790ecffa0cd2e03f721bcc0897a71fa23135355
irladii <spectradev17@gmail.com>
2026-01-31 13:50:48 +0530

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -1,11 +1,9 @@
-let data = {
-  A: [],
-  B: [],
-  C: [],
-  D: []
-};
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { group } = req.query;
-  res.json(data[group] || []);
+
+  const url = "https://raw.githubusercontent.com/irladii/spl-api/main/data/standings.json";
+  const r = await fetch(url);
+  const data = await r.json();
+
+  res.status(200).json(data[group] || []);
 }
